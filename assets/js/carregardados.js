0344551c1a20ac5a1e2a3bbf634cf33047429333
Dagmar Cirino <dagmar.mv@gmail.com>
2020-09-18 01:44:14 -0300

    Dados com informações em "tempo real"

---
+++ assets/js/carregardados.js
@@ -0,0 +1,22 @@
+
+function geral(categoriaDado, tipoDado, idCampo, frase){
+	var url = 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeralApi';
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4){
+			var dados = JSON.parse(xhr.responseText);
+			document.getElementById(idCampo).innerHTML = dados[categoriaDado][tipoDado] + frase;
+		}
+	};
+	xhr.open('GET', url);
+	xhr.send();
+}
+
+geral('confirmados', 'acompanhamento', 'dadoAcompanhamento', ' em monitoramento.');
+geral('confirmados', 'recuperados', 'dadoRecuperado', ' já se recuperaram.');
+geral('confirmados', 'novos', 'dadoNovos', ' novos casos.');
+geral('confirmados', 'total', 'dadoConfirmadoTotal', ' total notificados.');
+geral('obitos', 'novos', 'dadoObitosNovos', ' óbitos nas últimas 24h');
+geral('obitos', 'total', 'dadoObitosTotal', ' infelizmente vieram a óbito.');
+
+console.log('Feito por Cirino, durante as lives no canal https://twitch.tv/CasaDoDev');
