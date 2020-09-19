5b75828611f316ca0176b9cf10746eb01f06cf79
Dagmar Cirino <dagmar.mv@gmail.com>
2020-09-19 02:04:20 -0300

    Melhorando layout, seo e fastload

--- assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
+++ assets/web/assets/cookies-alert-plugin/cookies-alert-script.js
@@ -1,3 +1,14 @@
 jQuery(function(){var b=jQuery,a=b("input[name=cookieData]");b.cookiesDirective({customDialogSelector:"null"===a.attr("data-cookie-customDialogSelector")?null:a.attr("data-cookie-customDialogSelector"),explicitConsent:!1,position:"bottom",duration:0,limit:0,message:a.attr("data-cookie-text"),fontFamily:"Arial",fontColor:a.attr("data-cookie-colorText"),fontSize:"13px",backgroundColor:a.attr("data-cookie-colorBg"),backgroundOpacity:"",linkColor:a.attr("data-cookie-colorLink"),underlineLink:a.attr("data-cookie-underlineLink"),
 textButton:a.attr("data-cookie-textButton"),colorButton:a.attr("data-cookie-colorButton"),animate:"null"===a.attr("data-cookie-customDialogSelector")});a.remove()});
-!function(){try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.style="display: none";a.innerHTML='<a href="https://mobirise.in">Mobirise Easy Website Builder</a> Mobirise v5.1.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
+
+!function(){
+	try{document.getElementsById("top-1")[0].getElementsByTagName("a")[0].removeAttribute("rel")
+}catch(b){}
+
+// if(!document.getElementById("top-1")){
+// 	var a=document.createElement("section");
+// 	a.id="top-1";
+// 	a.style="display: none";
+// 	a.innerHTML='<a href="https://mobirise.in">Mobirise Easy Website Builder</a> Mobirise v5.1.4';document.body.insertBefore(a, document.body.childNodes[0])
+// }
+}();
