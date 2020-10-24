e249c992ef5a181106bb0a2a83b377323cee264e
Daniel Lisboa <daniel.videira@protonmail.com>
2020-10-24 01:13:38 -0300

    ajustando chamada da api de dados

--- assets/js/carregardados.js
+++ assets/js/carregardados.js
@@ -1,26 +1,27 @@
 // dados por estados
 // https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalEstado
 
-function geral(categoriaDado, tipoDado, idCampo, frase){
-	var url = 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeralApi';
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4){
-			var dados = JSON.parse(xhr.responseText);
-			var valor = parseInt((dados[categoriaDado][tipoDado]), 10).toLocaleString('pt-BR');
-			document.getElementById(idCampo).innerHTML = valor + frase;
-		}
-	};
-	xhr.open('GET', url);
-	xhr.send();
-}
+(function(){
+	const url = 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeralApi';
+	const dadosRequeridos = [
+		['confirmados', 'acompanhamento', 'dadoAcompanhamento', ' em monitoramento.'],
+		['confirmados', 'recuperados', 'dadoRecuperado', ' já se recuperaram.'],
+		['confirmados', 'novos', 'dadoNovos', ' novos casos.'],
+		['confirmados', 'total', 'dadoConfirmadoTotal', ' total notificados.'],
+		['obitos', 'novos', 'dadoObitosNovos', ' óbitos nas últimas 24h'],
+		['obitos', 'total', 'dadoObitosTotal', ' infelizmente vieram a óbito.'],
+		['obitos', 'letalidade', 'dadosLetalidade', '% Taxa de Letalidade.']
+	]
 
-geral('confirmados', 'acompanhamento', 'dadoAcompanhamento', ' em monitoramento.');
-geral('confirmados', 'recuperados', 'dadoRecuperado', ' já se recuperaram.');
-geral('confirmados', 'novos', 'dadoNovos', ' novos casos.');
-geral('confirmados', 'total', 'dadoConfirmadoTotal', ' total notificados.');
-geral('obitos', 'novos', 'dadoObitosNovos', ' óbitos nas últimas 24h');
-geral('obitos', 'total', 'dadoObitosTotal', ' infelizmente vieram a óbito.');
-geral('obitos', 'letalidade', 'dadosLetalidade', '% Taxa de Letalidade.');
+	fetch(url)
+		.then(res => res.json())
+		.then((out) => {
+			for (i = 0; i < dadosRequeridos.length; i ++){
+				var valor = parseInt((out[dadosRequeridos[i][0]][dadosRequeridos[i][1]]), 10).toLocaleString('pt-BR');
+				document.getElementById(dadosRequeridos[i][2]).innerHTML = valor + dadosRequeridos[i][3]
+			}
+		})
+		.catch(err => { throw err });
+}());
 
 console.log('Feito por Cirino, durante as lives no canal https://twitch.tv/CasaDoDev');
